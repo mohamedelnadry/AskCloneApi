25bf526f6159ffb96d1c908dffde8d623ecaee3a
mohamedelnadry <mohamdelnadry12345@gmail.com>
2023-07-04 04:44:48 +0300

    add

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Question
+from accounts.models import Profile
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    reciever = serializers.ListField(write_only=True)
+
     class Meta:
         model = Question
         fields = ["question_body", "sender", "reciever", "anonymous"]
+
+    def create(self, validated_data):
+        reciever_ids = validated_data.pop("reciever", [])
+        question = Question.objects.create(**validated_data)
+
+        receivers = Profile.objects.filter(user__username__in=reciever_ids)
+        for receiver in receivers:
+            question.reciever.add(receiver)
+
+        return question
